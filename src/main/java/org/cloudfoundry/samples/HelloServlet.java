2a7d6e6978c0fec8b692bc849fb434d08752d309
Saktheevel <saktheevel@rediffmail.com>
2014-11-09 13:02:05 -0600

    Revert "Added Comment"
    This reverts commit 2943c4774406c03e312d7a824942a392e29c38ad.

--- src/main/java/org/cloudfoundry/samples/HelloServlet.java
+++ src/main/java/org/cloudfoundry/samples/HelloServlet.java
@@ -24,7 +24,6 @@ public class HelloServlet extends HttpServlet {
 			int dockerIPAddressEnd = ssh_client_info.indexOf(" ", portEnd + 1);
 			ip_addr = ssh_client_info.substring(portEnd + 1, dockerIPAddressEnd);
 		}
-		//comments added
 		writer.println("Hello from " + ip_addr + ":" + System.getenv("VCAP_APP_PORT"));
 		writer.close();
 	}
